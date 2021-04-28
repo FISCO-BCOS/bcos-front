9c23285a52f77d81314c84c3db5f2311e741ec00
octopus <912554887@qq.com>
2021-04-28 10:43:14 +0800

    modify bcos-framework download url

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,5 +1,5 @@
 hunter_config(bcos-framework VERSION 3.0.0-03b469a
-URL https://codeload.github.com/ywy2090/bcos-framework/zip/refs/heads/dev
-SHA1 f707190ab8826f6d054ef382757c00c8cd011f11
+URL https://codeload.github.com/FISCO-BCOS/bcos-framework/zip/c5bbaf470e16fa4700b54a94cc19d3f3800439bd
+SHA1 56894eb56e57f27e88cd277b1a6196e3966b89fd
 CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
 )
