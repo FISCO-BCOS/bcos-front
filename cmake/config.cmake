85da4d8707b7cac4e8faab121f76afa48958fd00
octopus <912554887@qq.com>
2021-04-28 10:05:59 +0800

    add config.cmake

---
+++ cmake/config.cmake
@@ -0,0 +1,5 @@
+hunter_config(bcos-framework VERSION 3.0.0-03b469a
+URL https://codeload.github.com/ywy2090/bcos-framework/zip/refs/heads/dev
+SHA1 f707190ab8826f6d054ef382757c00c8cd011f11
+CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
+)
