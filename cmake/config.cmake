ff48bb989bec4dfab895095adc87a2277970fdea
morebtcg <651932351@qq.com>
2021-06-03 10:00:32 +0800

    change FETCH_URL_BASE to URL_BASE

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,5 +1,5 @@
 hunter_config(bcos-framework
 	VERSION 3.0.0-local
-	URL "https://${FETCH_URL_BASE}/FISCO-BCOS/bcos-framework/archive/8f36630305b653da7fa9e47c16b0550f08343d49.tar.gz"
+	URL "https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/8f36630305b653da7fa9e47c16b0550f08343d49.tar.gz"
 	SHA1 53135428ed54390f2207100fd56e8907ad3577dc
 	)
